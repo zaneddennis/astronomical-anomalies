d4b9858c1abcd56b84d866f149637a0c29c30d83
Zane Dennis <zaneddennis@gmail.com>
2020-12-01 09:06:58 -0500

    Plotting real labels

--- cluster_events.py
+++ cluster_events.py
@@ -1,14 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
 
 from sklearn.cluster import DBSCAN
 
 
-DATA_FILEPATH = "notebooks/mockdata.npz"
+SAMPLE_FILEPATH = "notebooks/mockdata.npz"
+FULL_FILEPATH = "notebooks/fulldata.npz"
+LONGCURVES_FILEPATH = "notebooks/fulldata_longcurves.npz"
 NUM_PASSBANDS = 6
+
+CLASS_NAMES = {
+    0: "SNI-Combined",
+    15: "TDE",
+    42: "SNII",
+    52: "SNIax",
+    62: "SNIbc",
+    67: "SNIa-91bg",
+    88: "AGN",
+    90: "SNIa",
+    95: "SLSN-I",
+    992: "ILOT",
+    993: "CaRT",
+    994: "PISN"
+}
 
 
 def plot_event(params, event_id, times, fluxes, filters, plot_regression=True):
@@ -57,7 +76,7 @@ def get_features(ix, times, fluxes, filters):
             X = np.array(e_fluxes[passband])
 
             linreg = LinearRegression()
-            linreg.fit(X[:, 0:1], X[:, 1:2])
+            linreg.fit(X[:, 0:1].astype(np.float32), X[:, 1:2])
 
             features[passband * 2] = linreg.coef_[0][0]
             features[1 + passband * 2] = linreg.intercept_[0]
@@ -65,12 +84,16 @@ def get_features(ix, times, fluxes, filters):
     return features
 
 
-def main(model, to_analyze=-1, plot_all=False, plot_outliers=False):
-    data = np.load(DATA_FILEPATH, allow_pickle=True)
+def main(model, filepath, to_analyze=-1, plot_all=False, plot_outliers=False,
+         plot_classes=False):
+    data = np.load(filepath, allow_pickle=True)
     times = data["times"]
     fluxes = data["fluxes"]
     flux_errors = data["flux_errs"]  # unused so far
     filters = data["filters"]
+    classes = None
+    if plot_classes:
+        classes = data["ids"]
 
     for i, e_times in enumerate(times):
         e_times = [t - e_times[0] for t in e_times]
@@ -106,19 +129,18 @@ def main(model, to_analyze=-1, plot_all=False, plot_outliers=False):
         for o in outliers:
             plot_event(X[o, :], o, times, fluxes, filters)
 
-    clusters, cl_counts = np.unique(labels, return_counts=True)
+    # plot clusters in 2D space
+    clusters, cl_ixes, cl_counts = np.unique(labels, return_index=True, return_counts=True)
     print("CLUSTERS:")
     print(clusters)
     print(cl_counts)
 
-    # plot clusters in 2D space
-    f1 = 2  # teal slope
-    f2 = 10  # red slope
+    f1 = 3  # teal intercept
+    f2 = 11  # red intercept
     feature1 = X[:, f1]
     feature2 = X[:, f2]
 
-
-
+    # plot clusters
     fig, ax = plt.subplots()
     for label in clusters:
         ixes = np.where(labels == label)[0]
@@ -126,13 +148,43 @@ def main(model, to_analyze=-1, plot_all=False, plot_outliers=False):
         points2 = feature2[ixes]
         ax.scatter(points1, points2, label=label)
 
-    plt.xlabel("Red Passband IR Slope")
-    plt.ylabel("Teal Passband IR Slope")
+    plt.xscale("symlog")
+    plt.yscale("symlog")
+    plt.xlabel("Red Passband IR Intercept")
+    plt.ylabel("Teal Passband IR Intercept")
     ax.legend()
     plt.show()
 
+    # plot labels
+    if plot_classes:
+        class_ids, class_ixes, class_counts = np.unique(classes[:to_analyze], return_index=True, return_counts=True)
+
+        print("CLASSES:")
+        print(class_ids)
+        print(class_counts)
+
+        fig, ax = plt.subplots()
+        for cid in class_ids:
+            ixes = np.where(classes[:to_analyze] == cid)[0]
+            points1 = feature1[ixes]
+            points2 = feature2[ixes]
+            ax.scatter(points1, points2, label=CLASS_NAMES[int(cid)])
+
+        plt.xscale("symlog")
+        plt.yscale("symlog")
+        plt.xlabel("Red Passband IR Intercept")
+        plt.ylabel("Teal Passband IR Intercept")
+        ax.legend()
+        plt.show()
+
+    matrix = metrics.cluster.contingency_matrix(classes[:to_analyze], labels)
+    df = pd.DataFrame(matrix, index=class_ids, columns=clusters)
+    print(df)
+    print("Normalized mutual info score:")
+    print(metrics.normalized_mutual_info_score(classes[:to_analyze], labels))
+
 
 if __name__ == '__main__':
-    dbscan = DBSCAN(eps=1.5, min_samples=5)
+    dbscan = DBSCAN(eps=0.7, min_samples=24)
 
-    main(dbscan, plot_all=False, plot_outliers=False)
+    main(dbscan, FULL_FILEPATH, to_analyze=50000, plot_all=False, plot_outliers=False, plot_classes=True)
